8b0ac87ef67055beec2fd2113e141dc1e9e096bb
sonyasimk <sonya.simkin@gmail.com>
2024-05-20 18:49:13 -0400

    revert

--- docs/src/js/app.js
+++ docs/src/js/app.js
@@ -16,7 +16,7 @@ document.addEventListener("DOMContentLoaded", function () {
         "value": "#ffffff"
       },
       "shape": {
-        "type": "image",
+        "type": "polygon",
         "stroke": {
           "width": 0,
           "color": "#000000"
